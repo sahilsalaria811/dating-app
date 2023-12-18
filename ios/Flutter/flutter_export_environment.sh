52ca69f92cbf07797fb99a6f7e84cb9f3b613ed7
Softradix <softradix@Softradixs-Mac-mini.local>
2023-12-18 17:09:36 +0530

    Project in new system

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/softradix/Documents/Flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/softradix/StudioProjects/Datingapp"
+export "FLUTTER_ROOT=/Users/softradix/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/softradix/Library/Mobile Documents/com~apple~CloudDocs/Documents/Flutter/Datingapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
